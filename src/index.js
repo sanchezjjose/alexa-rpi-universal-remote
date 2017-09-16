1303efa0bd0e5f5967f2e3105e496fdd39b4f4ba
josanche <jose.sanchez@mlb.com>
2017-09-15 23:10:39 -0400

    update intent schema

--- src/index.js
+++ src/index.js
@@ -33,10 +33,10 @@ const handlers = {
         const requestUrl = SERVER_URL + '/on' + query;
         
         return request(requestUrl, (error, response, body) => {
-            const outputSpeech = `Your unit is now on, and set to ${mode} on ${speed}, at ${temp} degrees.`;
+            console.log(body);
         
             if (!error && response.statusCode == 200) {
-                this.emit(':tell', outputSpeech);
+                this.emit(':tell', body);
 
             } else {
                 console.log('Request error: ', err);
@@ -57,10 +57,10 @@ const handlers = {
         const requestUrl = SERVER_URL + '/set' + query;
         
         return request(requestUrl, (error, response, body) => {
-            const outputSpeech = `Your unit is now set to ${mode} on ${speed}, at ${temp} degrees.`;
+            console.log(body);
         
             if (!error && response.statusCode == 200) {
-                this.emit(':tell', outputSpeech);
+                this.emit(':tell', body);
 
             } else {
                 console.log('Request error: ', err);
@@ -75,9 +75,10 @@ const handlers = {
         const requestUrl = SERVER_URL + '/off';
 
         return request(requestUrl, (error, response, body) => {
+            console.log(body);
         
             if (!error && response.statusCode == 200) {
-                this.emit(':tell', 'Your unit is now off.');
+                this.emit(':tell', body);
 
             } else {
                 console.log('Request error: ', err);
